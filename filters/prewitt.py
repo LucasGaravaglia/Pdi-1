952bb6479c3fc3f5f46b4d963dbfea4f18c4aef6
Lucas <lucasgrafimar@gmail.com>
2021-12-17 15:11:44 -0300

    a

--- filters/prewitt.py
+++ filters/prewitt.py
@@ -1,4 +1,5 @@
-from scipy import ndimage
+import cv2
+import numpy as np
 
 
 def prewittCv(image):
@@ -7,4 +8,8 @@ def prewittCv(image):
     Parameters:
     image -> imagem que recebera o filtro.
     """
-    return ndimage.prewitt(image)
+    kernelx = np.array([[1, 1, 1], [0, 0, 0], [-1, -1, -1]])
+    kernely = np.array([[-1, 0, 1], [-1, 0, 1], [-1, 0, 1]])
+    img_prewittx = cv2.filter2D(image, -1, kernelx)
+    img_prewitty = cv2.filter2D(image, -1, kernely)
+    return img_prewittx+img_prewitty
